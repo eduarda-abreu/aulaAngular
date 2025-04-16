c4911e408418d3210c56e471613771a20e2be6a9
Eduarda Abreu <gabreududa@gmail.com>
2025-04-16 19:51:45 -0300

    arrumando as coisas, pois mudei a ideia, agora o botão mudará de cor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,23 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "Contador de Cliques";
-  clickCount = 0;
+  contador = 0;
 
-  // Lista de imagens (use URLs locais ou online)
-  imagens: string[] = [
-    'assets/img1.jpg',
-    'assets/img2.jpg',
-    'assets/img3.jpg',
-    'assets/img4.jpg'
-  ];
+  cores = ['red', 'blue', 'green', 'yellow', 'purple'];
+  corAtual = this.cores[0];
 
-  get imagemAtual(): string {
-    return this.imagens[this.clickCount % this.imagens.length];
-  }
-
-  contarClique(): void {
-    this.clickCount++;
+  contarClique() {
+    this.contador++;
+    this.corAtual = this.cores[this.contador % this.cores.length];
   }
 }
-
